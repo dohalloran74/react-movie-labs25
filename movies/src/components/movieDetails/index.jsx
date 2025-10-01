e5ee255f8b785100d2332f0862b6c77416cfb6b7
deirdreOH <dohalloran@wit.ie>
2025-10-01 13:40:04 +0100

    Show movie Review excerpts; Refactor Details Page

--- movies/src/components/movieDetails/index.jsx
+++ movies/src/components/movieDetails/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router";
 import Chip from "@mui/material/Chip";
 import Paper from "@mui/material/Paper";
@@ -8,6 +8,10 @@ import StarRate from "@mui/icons-material/StarRate";
 import NavigationIcon from "@mui/icons-material/Navigation";
 import Fab from "@mui/material/Fab";
 import Typography from "@mui/material/Typography";
+
+import Drawer from "@mui/material/Drawer";
+import MovieReviews from "../movieReviews";
+
 
 const root = {
   display: "flex",
@@ -19,8 +23,9 @@ const root = {
 };
 const chip = { margin: 0.5 };
 
-const MovieDetails = (props) => {
-  const movie = props.movie;
+const MovieDetails = ({ movie }) => {
+  // Don't miss this!
+  const [drawerOpen, setDrawerOpen] = useState(false);
 
   return (
     <>
@@ -57,15 +62,23 @@ const MovieDetails = (props) => {
       <Fab
         color="secondary"
         variant="extended"
+        onClick={() => setDrawerOpen(true)}
         sx={{
           position: "fixed",
-          bottom: 2,
-          right: 2,
+          bottom: "1em",
+          right: "1em",
         }}
       >
         <NavigationIcon />
         Reviews
       </Fab>
+      <Drawer
+        anchor="top"
+        open={drawerOpen}
+        onClose={() => setDrawerOpen(false)}
+      >
+        <MovieReviews movie={movie} />
+      </Drawer>
     </>
   );
 };
